0e7ae95ae87e27d030c359659dd584bfb6e0ab8c
anupamb-2000 <anupam.b@inapp.com>
2022-08-06 02:11:13 +0530

    Added changes to JS Form Validation

--- JS Form Validation/scripts.js
+++ JS Form Validation/scripts.js
@@ -1,27 +1,79 @@
-var checkEmpty = function (field) {
-    if (field == "") {
-        return false;
-    }
-    return true;
-}
+const form = document.getElementById("form");
+const username = document.getElementById("name");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
+const pswd1 = document.getElementById("pswd1");
+const pswd2 = document.getElementById("pswd2");
 
-var validate = function (event) {
-    let name = document.getElementById("name");
-    console.log(name.value);
-    let email = document.getElementById("email");
-    console.log(email.value);
-    let phone = parseInt(document.getElementById("phone"));
-    console.log(phone.value);
-    let pswd1 = document.getElementById("pswd1");
-    console.log(pswd1.value);
-    let pswd2 = document.getElementById("pswd2");
-    console.log(pswd2.value);
-    let flname =  checkEmpty(name);
-    if (flname) {
-        document.getElementById("errname");
+console.log("Inside scripts.js")
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log("Inside eventListener");
+    checkInputs();
+});
+
+function checkInputs() {
+    const nameValue = username.value.trim();
+    const emailValue = email.value.trim();
+    const phoneValue = phone.value.trim();
+    const pswdValue = pswd1.value.trim();
+    const pswd2Value = pswd2.value.trim();
+
+    if(nameValue === '') {
+        setErrorFor(username, 'Name cannot be blank');
+    }else {
+        setSuccessFor(username);
     }
-    let flemail = checkEmpty(email);
-    let flphone = checkEmpty(phone);
-    let flpswd1 = checkEmpty(pswd1);
-    let flpswd2 = checkEmpty(pswd2);
+
+    if(emailValue === '') {
+        setErrorFor(email, 'Email cannot be blank');
+    }else if(!isEmail(emailValue)) {
+        setErrorFor(email, "Email is not valid");
+    }else{
+        setSuccessFor(email);
+    }
+
+    if(phoneValue === '') {
+        setErrorFor(phone, 'Phone number cannot be blank');
+    }else if(!phoneValue.match(/^\d{10}$/)) {
+        setErrorFor(phone, "Phone number is not valid")
+    }else {
+        setSuccessFor(phone);
+    }
+
+    if(pswdValue === '') {
+        setErrorFor(pswd1, 'Password cannot be blank');
+    }else if (!(pswd2Value.length >= 6 && pswd2Value.length <= 20)) {
+        setErrorFor(pswd1, 'Password must be 6-20 characters long');
+    }else {
+        setSuccessFor(pswd1);
+    }
+
+    if(pswd2Value === '') {
+        setErrorFor(pswd2, 'Password cannot be blank');
+    }else if (pswdValue !== pswd2Value) {
+        setErrorFor(pswd2, 'Passwords do not match');
+    }else {
+        setSuccessFor(pswd2);
+    }
+
+    function setErrorFor(input, message) {
+        const formControl = input.parentElement;
+        const small = formControl.querySelector("small");
+
+        small.innerText = message;
+
+        formControl.className = "form-control err";
+    }
+
+    function setSuccessFor(input) {
+        const formControl = input.parentElement;
+        formControl.className = "form-control success";
+    }
+
+    function isEmail(email) {
+        return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+    }
+
 }
